f856200a2719230dc749a1105bcbec78ece80d2e
Luciano Alzugaray <lucianoalzugaray1@gmail.com>
2017-09-14 02:15:40 -0300

    template login preparado. Falta arreglar imagen de fondo. Templating iniciado.

--- apps/login/models.py
+++ apps/login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=80)
+    username = models.CharField(max_length=30)
