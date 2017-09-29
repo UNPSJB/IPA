76cebcd972950c2a290ef5703344f938e0f08c5a
Luciano Alzugaray <lucianoalzugaray1@gmail.com>
2017-09-29 14:20:09 -0300

    maquetado copado del index iniciado

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,2 @@
 from django.db import models
-
-# Create your models here.
-class afluente(models.Model):
-	nombre= models.CharField()
-	superficie=
-	def __init__(self, arg):
-		super(afluente, self).__init__()
-		self.arg = arg
-
+from django.contrib.auth.models import AbstractUser
---
+++ assets/page/js/index.js
@@ -0,0 +1,11 @@
+$('.ui.dropdown.top')
+  .dropdown({
+  	on:'click'
+  })
+;
+
+$('.ui.dropdown')
+  .dropdown({
+  	on:'hover'
+  })
+;
