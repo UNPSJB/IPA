f4b0f477d83f34055b171080b5dd0cefa2902b57
Lucas <88lucas88@gmail.com>
2017-10-03 18:42:03 -0300

    Fix urls and comment Usuario Model

--- apps/tiposDocumentacion/urls.py
+++ apps/tiposDocumentacion/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'documentos'
 urlpatterns = [
 	url(r'^$', views.listar_tiposDocumentacion, name='index'),
-	url(r'^/alta', views.alta_tiposDocumentos, name='alta'),
-	url(r'^/listar$', views.listar_tiposDocumentacion, name='listar'),
+	url(r'^alta$', views.alta_tiposDocumentos, name='alta'),
+	url(r'^listar$', views.listar_tiposDocumentacion, name='listar'),
 	
 ]
--- apps/users/models.py
+++ apps/users/models.py
@@ -2,5 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from apps.personas.models import Rol
 
-class Usuario(Rol, AbstractUser):
-        pass
+#class Usuario(Rol, AbstractUser):
