b1003f74f3f255ce5cb258af253b820b95d43af2
Lucas <88lucas88@gmail.com>
2017-10-09 19:43:20 -0300

    apps afluente

--- apps/afluente/urls.py
+++ apps/afluente/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
-from .views import ListadoAfluentes, AltaAfluente
+from .views import ListadoAfluentes, AltaAfluente, Detalle_Afluente, AfluenteDelete
 
 urlpatterns = [
 
 	url(r'^alta_afluentes$', AltaAfluente.as_view(), name='alta_afluentes'),
+	url(r'^detalle_afluente/(?P<pk>.+)/$', Detalle_Afluente.as_view(), name='detalle_afluente'),
 	url(r'^listar_afluentes$', ListadoAfluentes.as_view(), name='listar_afluentes'),
+	url(r'^eliminar/(?P<pk>\d+)$', AfluenteDelete.as_view(), name='delete_afluente'),
 	
 ]
--- apps/afluente/views.py
+++ apps/afluente/views.py
@@ -1,30 +1,35 @@
 from .forms import *
 
-#from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy
 
 from .models import Afluente
 
-from django.views.generic import ListView,CreateView,DeleteView,UpdateView
+from django.views.generic import ListView,CreateView,DeleteView,DetailView
 
 class AltaAfluente(CreateView):
 	model = Afluente
 	form_class = AfluenteForm
 	template_name = 'afluentes/altaAfluente.html'
+	success_url = reverse_lazy('afluente:listar_afluentes')
+
+class Detalle_Afluente(DetailView):
+	model = Afluente
+	template_name = 'afluentes/detalle_afluente.html'		
 
 
 class ListadoAfluentes(ListView):
 	model = Afluente
 	#form_class = AfluenteForm
 	template_name = 'afluentes/listado.html'
+	context_object_name = 'afluentes'
 
 	def get_context_data(self, **kwargs):
 		context = super(ListadoAfluentes, self).get_context_data(**kwargs)
 		context['headers'] = {'Nombre', 'Localidad','Caudal'}
-		context['botones','url'] = {'Alta': 'alta_afluentes', 'Listado':'listar_afluentes'}
+		#context['botones'] = {'Alta': 'alta_afluentes', 'Listado':'listar_afluentes'}
 		return context
 
 class AfluenteDelete(DeleteView):
-	pass
-	#model = Afluente
-	#template_name = ''
-	#success_url = reverse_lazy('xx:xx')
+	model = Afluente
+	template_name = 'afluentes/delete.html'
+	success_url = reverse_lazy('afluente:listar_afluentes')
