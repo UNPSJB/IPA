997f73d11a6b560a933f09a277131e59946c0cde
AndreaBageler <andreabageler@gmail.com>
2017-10-10 09:45:18 -0300

    ata de documentacion en carpeta tipoDocumentacion

--- apps/afluente/views.py
+++ apps/afluente/views.py
@@ -9,7 +9,7 @@ from django.views.generic import ListView,CreateView,DeleteView,DetailView,Updat
 class AltaAfluente(CreateView):
 	model = Afluente
 	form_class = AfluenteForm
-	template_name = 'forms.html'
+	template_name = 'form.html'
 	success_url = reverse_lazy('afluente:listar_afluentes')
 
 
