35da1cb69bbe7133bdf7030b5041dc48017a38b1
Lucas <88lucas88@gmail.com>
2017-10-17 19:33:16 -0300

    permisos avances

--- apps/establecimientos/models.py
+++ apps/establecimientos/models.py
@@ -6,7 +6,7 @@ from apps.localidad.models import Localidad
 # Create your models here.
 class Establecimiento(models.Model):
 	duenio = models.ForeignKey(Persona)
-	codigoCatastral = models.CharField(max_length=100)
+	codigoCatastral = models.CharField(max_length=100, primary_key = True)
 	superficie = models.IntegerField()
 	nombre = models.CharField(max_length=50)
 	descripcion = models.CharField(max_length=100)
