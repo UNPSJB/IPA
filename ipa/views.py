056b8e12afa35132afcd10ea17e3e58c24cd4e62
Luciano Alzugaray <lucianoalzugaray1@gmail.com>
2017-10-20 17:07:05 -0300

    mas codigo para recuperacion

--- ipa/views.py
+++ ipa/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.contrib.auth import login
-from django.contrib.auth.views import LoginView, TemplateView
+from django.contrib.auth.views import LoginView, PasswordResetView 
 from django.shortcuts import render, redirect
 from apps.users.backend import UserBackend
 
@@ -23,5 +23,9 @@ class Login(LoginView):
 			return redirect(self.success_url)
 		return render(request, self.template_name, {'message_error': 'Usuario o contraseña incorrectos'})
 
-class PasswordRecovery(TemplateView):
-	pass
+class PasswordReset(PasswordResetView):
+	failure_url = reverse_lazy('login')
+
+	def form_invalid(self, form):
+		"""If the form is invalid, render the invalid form."""
+		return self.redirect(self.failure_url)
