80307def8a1f8bd9e801e2dff2347a45bda20c5e
Fede <fedec_tw@hotmail.com>
2017-10-27 16:53:55 -0300

    arreglo urls

--- apps/expediente/urls.py
+++ apps/expediente/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'expediente'
 urlpatterns = [
 	url(r'^alta$', views.AltaExpediente.as_view(), name='alta'),
-	url(r'^listar$', views.ListadoExpediente.as_view(), name='listado'),
+	url(r'^listar$', views.ListadoExpediente.as_view(), name='listar'),
 	url(r'^detalle/(?P<pk>.+)/$', views.DetalleExpediente.as_view(), name='detalle'),
 	url(r'^eliminar/(?P<pk>\d+)$', views.DeleteExpediente.as_view(), name='delete'),
 	
