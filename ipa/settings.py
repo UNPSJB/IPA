9d9deffc542b564dc8ed887ece8c2b47bd570ab6
Lucas <88lucas88@gmail.com>
2017-11-03 15:23:56 -0300

    setting db

--- ipa/settings.py
+++ ipa/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'ipa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ipaPrueba_db',
+        'NAME': 'ipa_db',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
