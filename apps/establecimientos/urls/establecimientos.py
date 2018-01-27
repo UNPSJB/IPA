1e66914e7adc77445cbf8bf26acdd2d1f8a5c0f3
Carolina Quinta <carol_abi@hotmail.com>
2018-01-26 23:24:39 -0300

    otorgados y completos

--- apps/establecimientos/urls/establecimientos.py
+++ apps/establecimientos/urls/establecimientos.py
@@ -7,5 +7,6 @@ urlpatterns = [
 	url(r'^alta$', AltaEstablecimiento.as_view(), name='alta'),
 	url(r'^listar$', ListadoEstablecimientos.as_view(), name='listar'),
 	url(r'^detalle/(?P<pk>.+)/$', DetalleEstablecimiento.as_view(), name='detalle'),
-	url(r'^eliminar/(?P<pk>.+)$', DeleteEstablecimiento.as_view(), name='eliminar'),
+	url(r'^modificar/(?P<pk>\d+)$', ModificarEstablecimiento.as_view(), name='modificar'),
+	url(r'^eliminar/(?P<pk>\d+)$', DeleteEstablecimiento.as_view(), name='eliminar'),
 ]
