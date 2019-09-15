66d6ce77a8aba4045e889c2f84dab7b4b4dcbf48
Luciano Alzugaray <lucianoalzugaray1@gmail.com>
2019-09-15 17:39:41 -0300

    Retrieve de empresas dinamico en alta personas

--- apps/personas/urls/empresas.py
+++ apps/personas/urls/empresas.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from ..views.empresas import AltaEmpresa
+from ..views.empresas import AltaEmpresa, DataEmpresas
 
 app_name = 'empresas'
 urlpatterns = [
-	url(r'^alta$', AltaEmpresa.as_view(), name='alta')
+	url(r'^alta$', AltaEmpresa.as_view(), name='alta'),
+	url(r'^data$', DataEmpresas.as_view(), name='data')
 ]
--- apps/personas/views/empresas.py
+++ apps/personas/views/empresas.py
@@ -3,6 +3,11 @@ from apps.personas import models as pmodels
 from apps.personas.forms import *
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from apps.generales.views import GenericAltaView
+from django.http import JsonResponse
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
+
+from django.views import View
 
 class AltaEmpresa(GenericAltaView):
 	model = pmodels.Empresa
@@ -11,3 +16,11 @@ class AltaEmpresa(GenericAltaView):
 	success_url = reverse_lazy('personas:listado')
 	message_error = "Empresa existente"
 	cargar_otro_url = reverse_lazy('empresas:alta')
+
+class DataEmpresas(View):
+	def get(self, request, *args, **kwargs):
+		empresas = pmodels.Empresa.objects.all()
+		empresaDict = []
+		for empresa in empresas:
+			empresaDict.append({"razonSocial": empresa.razonSocial, "cuit": empresa.cuit})
+		return JsonResponse({"data": empresaDict})
